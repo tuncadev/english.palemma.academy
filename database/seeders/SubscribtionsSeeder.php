a4db43012dc9f2e088a2cfa7649090c25eb535a0
tuncatech <tunca.development@gmail.com>
2024-06-04 16:49:02 +0300

    subscribtions

--- database/seeders/SubscribtionsSeeder.php
+++ database/seeders/SubscribtionsSeeder.php
@@ -5,14 +5,15 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
-class SubscriptionsTableSeeder extends Seeder
+
+class SubscribtionsSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
     public function run(): void
     {
-        DB::table('subscriptions')->insert([
+        DB::table('subscribtions')->insert([
             [
                 'user_id' => 1,
                 'course_id' => 1,
@@ -20,7 +21,7 @@ class SubscriptionsTableSeeder extends Seeder
                 'subscription_date' => now(),
                 'expiry_date' => now()->addYear(),
                 'created_at' => now(),
-                'updated_at' => now(),
+
             ],
             [
                 'user_id' => 2,
@@ -29,7 +30,7 @@ class SubscriptionsTableSeeder extends Seeder
                 'subscription_date' => now(),
                 'expiry_date' => now()->addYear(),
                 'created_at' => now(),
-                'updated_at' => now(),
+
             ],
             [
                 'user_id' => 3,
@@ -38,9 +39,9 @@ class SubscriptionsTableSeeder extends Seeder
                 'subscription_date' => now(),
                 'expiry_date' => now()->addYear(),
                 'created_at' => now(),
-                'updated_at' => now(),
+
             ],
             // Add more seed data as needed
         ]);
     }
- }
+}
