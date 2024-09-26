9713d9071bd3e72bd3768f4156eaef840e69aa9f
tuncatech <tunca.development@gmail.com>
2024-09-26 17:05:56 +0300

    logo style

--- resources/views/components/top-logo.blade.php
+++ resources/views/components/top-logo.blade.php
@@ -1,4 +1,4 @@
-<a href="{{ url('/') }}" class="shadow-sm hover:shadow-lg flex px-2 py-1 rounded items-center space-x-3 rtl:space-x-reverse bg-pink-700">
+<a href="{{ url('/') }}" class="shadow-sm hover:shadow-lg flex px-3 py-1 rounded items-center space-x-3 rtl:space-x-reverse bg-pink-700">
     <div class="bg-white mr-2 px-1 md:px-2 md:py-1 rounded ">
         <i class="text-sm fa-solid fa-graduation-cap text-pink-700"></i>
     </div>
