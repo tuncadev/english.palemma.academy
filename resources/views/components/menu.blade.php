63779eef94519811332fe97cf161590fafa2bf9d
tuncatech <tunca.development@gmail.com>
2024-09-26 11:45:49 +0300

    menu links

--- resources/views/components/menu.blade.php
+++ resources/views/components/menu.blade.php
@@ -21,13 +21,13 @@
 </ul>
     <ul class="md:hidden flex md:w-full m-auto max-w-96 flex-wrap md:flex-nowrap  justify-center text-sm  flex-row font-medium md:p-0 border border-gray-100 rounded-lg rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
       <li class="pt-2 md:hidden border-sky-200 border-t-4 border-r border-r-1 border-sky-200">
-        <a href="#" class="flex flex-col items-center text-xs py-2 px-4 text-gray-600 rounded md:bg-transparent md:hover:text-blue-700  md:dark:text-blue-500" aria-current="page">
+        <a href="{{ route('dashboard.courses') }}" class="flex flex-col items-center text-xs py-2 px-4 text-gray-600 rounded md:bg-transparent md:hover:text-blue-700  md:dark:text-blue-500" aria-current="page">
             <i class="fa-solid fa-person-chalkboard text-fuchsia-800"></i>
           @lang('usermenu.mycourses')
         </a>
       </li>
       <li class="pt-2 md:hidden border-sky-200 border-t-4 border-r border-r-1 border-sky-200" >
-        <a href="#" class="flex flex-col items-center text-xs  py-2 px-4 text-gray-600 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
+        <a href="{{ route('profile.edit')}}" class="flex flex-col items-center text-xs  py-2 px-4 text-gray-600 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
             <i class="fa-solid fa-gears text-fuchsia-800"></i>
           @lang('usermenu.profile')
         </a>
