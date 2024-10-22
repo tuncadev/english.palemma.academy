0a47f31b87afc3b7ca5dc1cf9d11ee7f05962dfb
tuncatech <tunca.development@gmail.com>
2024-10-22 17:49:31 +0300

    login logo

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -1,4 +1,6 @@
-<a href="{{ url('/') }}" class="flex items-center space-x-3 rtl:space-x-reverse">
-    <img src="{{ asset('images/logo.jpg') }}" class="h-8 ring-2 rounded-full p-px ring-gray-400" alt="Palemma Logo" />
-    <span class="self-center text-xs  font-semibold whitespace-nowrap dark:text-white">Palemma Academy</span>
+<a href="{{ url('/') }}" class="shadow-sm hover:shadow-lg flex px-3 py-1 rounded items-center space-x-3 rtl:space-x-reverse bg-pink-700">
+    <div class="bg-white mr-2 px-1 md:px-2 md:py-1 rounded ">
+        <i class="text-sm fa-solid fa-graduation-cap text-pink-700"></i>
+    </div>
+    <span class="text-white self-center text-xs md:text-sm  font-semibold whitespace-nowrap dark:text-white">Palemma Academy</span>
 </a>
