b60b8b5d5881cb8d4796fafd927eaa3aa5f56787
tuncatech <tunca.development@gmail.com>
2024-10-22 17:44:54 +0300

    finishing

--- resources/views/components/sections-footer.blade.php
+++ resources/views/components/sections-footer.blade.php
@@ -2,6 +2,16 @@
 <x-info-msg message_bold="{{ __('course.available_message') }}" />
 <div class="flex flex-wrap items-center justify-start gap-2">
     <span class="text-xs">@lang('course.available_sections'):</span>
+    <a href="{{ route('course.introduction', ['course_id' => $course->id])}}">
+        <span class="hover:bg-amber-300 px-2 py-1 bg-amber-200 rounded text-xs">
+            @lang('course.intro')
+        </span>
+    </a>
+    <a href="{{ route('course.tutorials', ['course_id' => $course->id])}}">
+        <span class="hover:bg-pink-300 px-2 py-1 bg-pink-200 rounded text-xs">
+            @lang('course.video_intro')
+        </span>
+    </a>
     @foreach ($course->allSections as $section)
         @if ( !in_array($section->id - 1, $course->completedSections ) && $section->id != 1 )
             <span class="hover:cursor-default px-2 py-1 bg-gray-200 rounded text-xs">{{$section->section_name_en}}</span>
