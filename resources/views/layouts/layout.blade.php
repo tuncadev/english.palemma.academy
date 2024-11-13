ef517500aff183cc5a5fbdfda4ebc9f2e00a1ee6
tuncatech <tunca.development@gmail.com>
2024-11-13 08:48:10 +0200

    google tags

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -5,6 +5,9 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
     </head>
     <body class="bg-gray-100 p-0 m-0 relative flex flex-col h-screen justify-between">
+    <!-- Google Tag Manager (noscript) -->
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-T6QC54VR" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
         @php
             if(!isset($error)) { $error = ""; }
         @endphp
--- resources/views/layouts/navigation.blade.php
+++
@@ -1,7 +0,0 @@
-@php
-    $locale = session('locale', 'uk');
-    $currentLocale = session('locale', 'uk');
-@endphp
-    <div class="relative w-full bg-top_bar shadow-md l1_top">
-        <x-nav :locale="$locale" :currentLocale="$currentLocale" />
-    </div>
